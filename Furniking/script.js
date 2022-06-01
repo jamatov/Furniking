75969c55e7b30541a633a8adf236ca953b72fb97
Abdulla <106698312+jamatov@users.noreply.github.com>
2022-06-01 23:43:27 +0500

    Add files via upload

---
+++ Furniking/script.js
@@ -0,0 +1,12 @@
+let data = new Date()
+
+let minute = data.getMinutes()
+let second = data.getSeconds()
+
+document.getElementById('minute').innerHTML = minute
+document.getElementById('second').innerHTML = second
+
+document.getElementById('minute2').innerHTML = minute
+document.getElementById('second2').innerHTML = second
+
+
